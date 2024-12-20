7e8aa57005fd87634c2765430ff331fcca49a85c
vyominiiiiiiii <102034432+vyominiiiiiiii@users.noreply.github.com>
2024-12-20 11:43:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+var value1=parseInt(prompt("Enter the number"));
+var num=0;
+
+//WHILE LOOP
+while(value1>0){
+    console.log("value1= ",value1);
+    num+=value1;
+    console.log("num= ",num);
+    value1=parseInt(prompt("Enter the number"));
+    }
+
+//DO WHILE LOOP
+/*do{
+    console.log("value1= ",value1);
+    num+=value1;
+    console.log("num= ",num);
+    value1=parseInt(prompt("Enter the number"));
+}
+while(value1>0);*/
+
+
+
+
+
